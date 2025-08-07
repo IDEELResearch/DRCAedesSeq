faca9715c82c7de57e0a07f476112543b43cd9cb
Wenqiao He <153768872+Wenqiao33@users.noreply.github.com>
2025-08-07 16:26:22 -0400

    Add files via upload

---
+++ Nanopore_sequencing_data_visualization.R
@@ -0,0 +1,48 @@
+###########################################################################################################################################################################
+# This is an R script
+#Programmer: Wenqiao He
+#Last update: July 2025
+#Purpose: Visualization of Aedes mosquito nanopore targeted sequencing data analysis
+###########################################################################################################################################################################
+
+library(writexl)
+library(gplots)
+library(dplyr)
+library(ggplot2)
+
+### Plotting blood meal analysis results
+Blood_meal_annotation <- read_excel("C:/Users/wenqiao/Mosquito viral metagenomic/uploaded script/revision/Blood_meal_analysis_example_data.xlsx")
+
+Blood_meal_annotation_plot <- ggplot(Blood_meal_annotation, aes(x = Blood_meal_annotation$`Sample name`, y = Genus)) + 
+  geom_point(
+    aes(
+      fill = factor(Detection), 
+      color = factor(Detection)
+    ), 
+    size = 12, shape = 22  # shape 22 = filled square
+  ) +
+  scale_color_manual(values = c(
+    "1" = "red", 
+    "0" = "black"
+  )) +
+  scale_fill_manual(values = c(
+    "1" = "red", 
+    "0" = "white"
+  )) +
+  labs(
+    x = "Mosquito pool", 
+    color = "Detection", 
+    fill = "Detection"
+  ) +
+  theme_bw() +
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.text.x = element_text(size = 10),
+    axis.text.y = element_text(size = 10, face = "italic"),
+    axis.title = element_text(size = 12),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 12)
+  )
+
+Blood_meal_annotation_plot
