aa5cb0b2c589eb6d700d69e9bcdaf8ba5bd2c516
Wenqiao He <153768872+Wenqiao33@users.noreply.github.com>
2025-08-08 14:39:55 -0400

    Update Metagenomic data visualization (filtering).R

--- Metagenomic data visualization (filtering).R
+++ Metagenomic data visualization (filtering).R
@@ -52,10 +52,10 @@ library(ggnewscale)
 library(scico)
 
 ### Set working directory###
-setwd("/PATH/")
+setwd("/example data/")
 
-### Load data for heatmap at family level
-family_RPM <- read_excel("family_filtering.xlsx")
+### Load data for heatmap at the family level
+family_RPM <- read_excel("family_heatmap_filtering.xlsx")
 family_long <- family_RPM %>%
   pivot_longer(
     cols = -taxName,
@@ -72,8 +72,8 @@ ggplot(family_long, aes(x = Sample, y = taxName, fill = RPM_log)) +
   theme(axis.text.y = element_text(size=12, face = "italic"))
 
 
-### Load data for heatmap at genus level
-genus_RPM <- read_excel("genus_filtering.xlsx")
+### Load data for heatmap at the genus level
+genus_RPM <- read_excel("genus_heatmap_filtering.xlsx")
 genus_long <- genus_fltering %>%
   pivot_longer(
     cols = -taxName,
@@ -89,7 +89,7 @@ ggplot(genus_long, aes(x = Sample, y = taxName, fill = RPM_log)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   theme(axis.text.y = element_text(size=12, face = "italic"))
 
-### Load data for viral relative abundance analysis at family level 
+### Relative abundance analysis at the family level 
 family_RA <- family_RPM
 family_RA[, -1] <- lapply(family_RA[, -1], function(x) as.numeric(x) / 1e6 * 100)
 family_RA_long_table <- family_RA %>%
@@ -113,7 +113,7 @@ family_bar<-ggplot(data=family_RA_long_table, aes(x=Sample, y=family_RA_long_tab
         legend.title = element_text(size = 14))   
 family_bar
 
-### Load data for viral relative abundance analysis at genus level 
+### Relative abundance analysis at the genus level 
 genus_RA <- genus_RPM
 genus_RA[, -1] <- lapply(genus_RA[, -1], function(x) as.numeric(x) / 1e6 * 100)
 
@@ -138,8 +138,8 @@ genus_bar<-ggplot(data=genus_RA_long_table, aes(x=Sample, y=genus_RA_long_table$
         legend.title = element_text(size = 14))   
 genus_bar
 
-### PCA analysis based on viral genus annotation
-PCA_df <- read_excel("PCA_genus_filtering.xlsx")
+### PCA analysis based on the viral genus annotation
+PCA_df <- read_excel("PCA_analysis_genus_filtering.xlsx")
 PCAdf <- as.data.frame(PCA_df)
 rownames(PCAdf) <- PCAdf[,1]
 PCAdf <- PCAdf[,-1]
@@ -208,7 +208,7 @@ sort(abs(out.pca$rotation[,2]), decreasing = TRUE)[1:10]
 
 
 ### Plotting BLAST and KrakenUniq results for huamn and animal viruses
-blast_result <- read_excel("BLAST_confirmation_filtering.xlsx")
+blast_result <- read_excel("BLAST_KrakenUniq_results_filtering.xlsx")
 
 annotation_plot <- ggplot(blast_result, aes(x = Sample, y = Viruses, fill = Annotation, color = Annotation)) + 
   geom_point(size = 12.5, shape = 22) +
@@ -238,19 +238,21 @@ annotation_plot <- ggplot(blast_result, aes(x = Sample, y = Viruses, fill = Anno
 
 annotation_plot
 
-###Pan-DENV reference selection, download, and phylogenetic tree visualization
-##References (>10,000 bp) representing different genotypes were randomly selected from various geographic regions.  
-##The file "all_ref.xlsx" contains all DENV reference sequences from the NCBI Virus database, downloaded on June 25, 2025. 
+### Pan-DENV reference selection, download, and phylogenetic tree visualization
+## References (>10,000 bp) representing different genotypes were randomly selected from various geographic regions.  
+## The file "all_ref.xlsx" contains information on all published DENV sequences from the NCBI Virus database, downloaded on June 25, 2025. 
 all_ref <- read_excel("all_ref.xlsx")
 all_ref <- all_ref %>%
   mutate(Organism_Name = str_replace(Organism_Name, "dengue virus type I", "dengue virus type 1"))%>%
   mutate(Organism_Name = str_replace(Organism_Name, "dengue virus", "Dengue virus"))
 
+## Select the longest reference sequences representing different countries and collection years
 result <- all_ref %>%
   group_by(Collection_year, Country, Organism_Name) %>%
   slice_max(order_by = Length, n = 1, with_ties = FALSE) %>%
   ungroup()
 
+## Add continent information 
 countries <- ne_countries(returnclass = "sf") %>%
   sf::st_drop_geometry() %>%
   select(name, region_un, subregion,continent = continent) %>%
@@ -259,18 +261,18 @@ countries <- ne_countries(returnclass = "sf") %>%
 result_with_continent <- result%>%
   left_join(countries, by = "Country")
                    
-##References were selected to ensure representation across diverse geographical regions and serotypes.
+## Select references to ensure representation across diverse geographical regions and serotypes.
 random_all_ref <- result_with_continent %>%
   filter(Length >= 10000) %>%                               # Remove reads with length < 10000
   group_by(continent, Organism_Name) %>%                    # Group by region and serotype
   slice_sample(n = 3) %>%                                   # Randomly select 3 reads per group
   ungroup()
                    
-##Here is the final list of references included in the Pan-DENV phylogenetic analysis. Some references were removed due to high diversity compared to others of the same serotype. A Zika virus sequence was included as the outgroup.)
+## Final list of reference sequences included in the Pan-DENV phylogenetic analysis conducted in this study. Some references were excluded due to high genetic diversity compared to others of the same serotype. A Zika virus sequence was used as the outgroup.
 pan_denv_refs <- read_excel("DENV_all_serotypes_metadata.xlsx")
 pan_denv_published_refs <- subset(pan_denv_refs,pan_denv_refs$This_study=="no") 
 
-##Download references from NCBI 
+## Download references from the NCBI database
 acc_all <- pan_denv_published_refs$Accession
 fasta_seqs <- lapply(acc_all, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -279,7 +281,7 @@ fasta_seqs <- lapply(acc_all, function(acc) {
 combined_fasta <- unlist(fasta_seqs)
 writeLines(combined_fasta, "Pan_DENV_all_refs.fasta")
 
-##Load the phylogenetic tree for visualization
+## Load the phylogenetic tree for visualization
 Pan_DENV_raxmlng <- read.tree("DENV_all_serotypes_raxml_ng.raxml.support")
 Pan_DENV_raxmlng_tree <- ggtree(all_serotypes_raxmlng) %<+% pan_denv_refs +
   geom_tiplab(aes(color = This_study), size = 2.5) +
@@ -322,7 +324,7 @@ Pan_DENV_merge_2 <- gheatmap(
 
 Pan_DENV_merge_2
 
-###DENV-2 reference selection, download, and phylogenetic tree visualization
+### DENV-2 reference selection, download, and phylogenetic tree visualization
 ## Add continent information
 all_ref_with_continent <- all_ref%>%
   left_join(countries, by = "Country")
@@ -330,22 +332,22 @@ DENV2_ref <- all_ref_with_continent %>%
   filter(Length >= 10000) %>% # Remove reads with length < 10000
   subset(Organism_Name == "Dengue virus type 2")
 
-##Select the longest one from each country and year
+## Select the longest sequence representing each country and collection year
 DENV2_longest <- DENV2_ref %>%
      group_by(Collection_year, Country) %>%
      slice_max(order_by = Length, n = 1, with_ties = FALSE) %>%
      ungroup()
 
-##Select references from different geographical regions
+## Select reference sequences representing diverse geographic regions
 DENV2_random_ref <- DENV2_longest %>%
-  group_by( continent) %>%
+  group_by(continent) %>%
   slice_sample(n=15) %>%
   ungroup()
 
-##This is the final list of references included in the DENV-2 phylogenetic analysis. Some were excluded due to high genetic divergence. References with genotype information from a published study (PMID: 34188091) were included. A DENV-4 sequence was used as the outgroup.
+## Final list of references included in the DENV-2 phylogenetic analysis. Some references with genotype information from a published study (PMID: 34188091) were included. A DENV-4 sequence was used as the outgroup.
 denv2_refs <- read_excel("DENV2_metadata.xlsx")
 denv2_published_refs <- subset(denv2_refs,denv2_refs$This_study=="no") 
-##Download references from NCBI 
+## Download references from NCBI 
 acc_denv2 <- denv2_published_refs$Accession
 fasta_seqs_denv2 <- lapply(acc_denv2, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -354,7 +356,7 @@ fasta_seqs_denv2 <- lapply(acc_denv2, function(acc) {
 combined_fasta_denv2 <- unlist(fasta_seqs_denv2)
 writeLines(combined_fasta_denv2, "DENV2_all_refs.fasta")
 
-##Load phylogenetic tree for visualization
+## Load phylogenetic tree for visualization
 DENV2_raxmlng_tree <- read.tree("DENV2_raxml_ng.raxml.support")
 DENV2_tree <- ggtree(DENV2_raxmlng_tree)%<+% denv2_refs +
   geom_tiplab(aes(color = This_study), size = 2.5) +
@@ -408,28 +410,28 @@ DENV2_merge_2 <- gheatmap(
 
 DENV2_merge_2
 
-###DENV-4 reference selection, download, and phylogenetic tree visualization
+### DENV-4 reference selection, download, and phylogenetic tree visualization
 ## Add continent information
 DENV4_ref <- all_ref_with_continent %>%
   filter(Length >= 10000) %>% # Remove reads with length < 10000
   subset(Organism_Name == "Dengue virus type 4")
 
-##Select the longest one from each country and year
+## Select the longest sequence representing each country and collection year
 DENV4_longest <- DENV4_ref %>%
   group_by(Collection_year, Country) %>%
   slice_max(order_by = Length, n = 1, with_ties = FALSE) %>%
   ungroup()
 
-##Select references from different geographical regions
+## Select reference sequences representing diverse geographic regions
 DENV4_random_ref <- DENV4_longest %>%
   group_by( continent) %>%
   slice_sample(n=20) %>%
   ungroup()
 
-##This is the final list of references included in the DENV-4 phylogenetic analysis. Some were excluded due to high genetic divergence. References with genotype information from a published study (PMID: 37293986) were included. A DENV-2 sequence was used as the outgroup.
+## Final list of references included in the DENV-2 phylogenetic analysis. Some references with genotype information from a published study (PMID: 37293986) were included. A DENV-2 sequence was used as the outgroup.
 denv4_refs <- read_excel("DENV4_metadata.xlsx")
 denv4_published_refs <- subset(denv4_refs,denv4_refs$This_study=="no") 
-##Download references from NCBI 
+## Download references from the NCBI database 
 acc_denv4 <- denv4_published_refs$Accession
 fasta_seqs_denv4 <- lapply(acc_denv4, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -438,7 +440,7 @@ fasta_seqs_denv4 <- lapply(acc_denv4, function(acc) {
 combined_fasta_denv4 <- unlist(fasta_seqs_denv4)
 writeLines(combined_fasta_denv4, "DENV4_all_refs.fasta")
 
-##Load phylogenetic tree for visualization
+## Load phylogenetic tree for visualization
 DENV4_raxmlng_tree <- read.tree("DENV4_raxml_ng.raxml.support")
 DENV4_tree <- ggtree(DENV4_raxmlng_tree)%<+% denv4_refs +
   geom_tiplab(aes(color = This_study), size = 2.5) +
@@ -491,11 +493,11 @@ DENV4_merge_2 <- gheatmap(
 
 DENV4_merge_2
 
-###Bat faecal associated dicistrovirus 4 reference (dicistrovirus sequences from differnet hosts) download and phylogenetic tree visualization
+### Bat faecal associated dicistrovirus 4 reference (dicistrovirus sequences from differnet hosts) download and phylogenetic tree visualization
 bat_dic_refs <- read_excel("bat_dic_metadata.xlsx")
 bat_dic_published_refs <- subset(bat_dic_refs,bat_dic_refs$This_study=="no") 
 
-##Download references from NCBI 
+## Download references from the NCBI database 
 acc_bat_dic <- bat_dic_published_refs$Accession
 fasta_seqs_bat_dic <- lapply(acc_bat_dic, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -504,7 +506,7 @@ fasta_seqs_bat_dic <- lapply(acc_bat_dic, function(acc) {
 combined_fasta_bat_dic <- unlist(fasta_seqs_bat_dic)
 writeLines(combined_fasta_bat_dic, "bat_dic_all_refs.fasta")
 
-##Load phylogenetic tree for visualization
+## Load phylogenetic tree for visualization
 bat_dic_metadata <- read_excel("bat_dic_metadata.xlsx")
 bat_dic <- read.tree("Bat_dic_raxml_ng.raxml.support")
 bat_dic_tree <- ggtree(bat_dic)%<+% bat_dic_metadata +
@@ -537,10 +539,11 @@ bat_dic_tree_merge_1 <- gheatmap(
 
 bat_dic_tree_merge_1
 
-###Human blood-associated dicistrovirus reference (dicistrovirus sequences from different hosts) download and phylogenetic tree visualization
+### Human blood-associated dicistrovirus reference (dicistrovirus sequences from different hosts) download and phylogenetic tree visualization
 human_dic_refs <- read_excel("human_dic_metadata.xlsx")
 human_dic_published_refs <- subset(human_dic_refs,bat_dic_refs$This_study=="no") 
-##Download references from NCBI 
+
+## Download references from NCBI 
 acc_human_dic <- human_dic_published_refs$Accession
 fasta_seqs_human_dic <- lapply(acc_human_dic, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -549,7 +552,7 @@ fasta_seqs_human_dic <- lapply(acc_human_dic, function(acc) {
 combined_fasta_human_dic <- unlist(fasta_seqs_human_dic)
 writeLines(combined_fasta_human_dic, "human_dic_all_refs.fasta")
 
-##Load metadata for phylogenetic tree visualization. Three separate trees were generated for Human blood-associated dicistrovirus, as reads mapped to different regions of the genome.
+## Load the phylogenetic tree for visualization. Three separate trees were generated for human blood-associated dicistrovirus because reads mapped to different regions of the genome.
 human_dic_metadata <- read_excel("human_dic_metadate.xlsx")
 human_dic_K5_1 <- read.tree("Human_dic_K5_1_raxml_ng.raxml.support")
 human_dic_K5_1_tree <- ggtree(human_dic_K5_1)%<+% human_dic_metadata+
@@ -634,11 +637,11 @@ human_dic_K4_tree_merge_1 <- gheatmap(
 human_dic_K4_tree_merge_1
 
 
-###Human pegivirus reference (pegivirus sequences from different hosts) download and phylogenetic tree visualization
+### Human pegivirus reference (pegivirus sequences from different hosts) download and phylogenetic tree visualization
 pegivirus_refs <- read_excel("Pegivirus_metadata.xlsx")
 pegivirus_published_refs <- subset(pegivirus_refs,pegivirus_refs$This_study=="no") 
 
-##Download references from NCBI 
+## Download references from the NCBI database 
 acc_pegivirus <- pegivirus_published_refs$Accession
 fasta_seqs_pegivirus <- lapply(acc_pegivirus, function(acc) {
   entrez_fetch(db = "nuccore", id = acc, rettype = "fasta")
@@ -647,7 +650,7 @@ fasta_seqs_pegivirus <- lapply(acc_pegivirus, function(acc) {
 combined_fasta_pegivirus <- unlist(fasta_seqs_pegivirus)
 writeLines(combined_fasta_pegivirus, "pegivirus_all_refs.fasta")
 
-##Load metadata for phylogenetic tree visualization
+## Load phylogenetic tree for visualization
 pegivirus_metadata <- read_excel("pegivirus_metadata.xlsx")
 pegivirus_host_data <- as.data.frame(pegivirus_metadata[, c("Host","Accession")])
 rownames(pegivirus_host_data) <- pegivirus_host_data$Accession
@@ -678,6 +681,3 @@ Pegivirus_tree_merge_1 <- gheatmap(
     na.value = "gray90"
   )
 Pegivirus_tree_merge_1
-
-
-
